5b131caa40d727a83a85f4e3ccaff867db193e0e
raul monroy <raulisai97@gmail.com>
2025-05-21 20:15:28 -0600

    docs

---
+++ docs/_app/immutable/chunks/Ge17H7du.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_m4nely)==null?void 0:s.base)??"/miweb";var e;const t=((e=globalThis.__sveltekit_m4nely)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ docs/_app/immutable/entry/start.DOX8jIc4.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Di4k_5Lm.js";export{o as load_css,r as start};
